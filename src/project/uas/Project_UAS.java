89341d8092c58cca6cfb7d82747eeb23c6d73a5f
ASUS <ASUS@DESKTOP-2PPTLSD>
2024-02-24 19:26:37 +0700

    First commit

---
+++ src/project/uas/Project_UAS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package project.uas;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Project_UAS {
+
+    public static void main(String[] args) {
+        new FormUtama().setVisible(true);
+    }
+}
